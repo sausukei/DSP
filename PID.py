b40285529f702432056785585bedc6e290a98348
Ikeno Sosuke <ikenosousuke@icloud.com>
2023-10-05 23:55:44 +0900

    bit change

--- PID.py
+++ PID.py
@@ -6,10 +6,16 @@ import matplotlib.pyplot as plt
 omegan = 2.0/100.0
 zeta = 2.0/100.0
 
+rt = 1
+dt = 2.5
+t = np.arange(-1,25*dt,dt)
+
+
+
 Kp = int(input("Kp "))
 Ki = int(input("Ki "))
 Kd = int(input("Kd "))
 
-print(Kp+Ki+Kd)
+
 
 
