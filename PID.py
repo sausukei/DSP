5d96302ec56f4bbc92116f73ca6e5916496cbaaa
Ikeno Sosuke <ikenosousuke@icloud.com>
2023-10-06 21:00:15 +0900

    bit change

--- PID.py
+++ PID.py
@@ -8,14 +8,19 @@ zeta = 2.0/100.0
 
 rt = 1
 dt = 2.5
-t = np.arange(-1,25*dt,dt)
+t = np.arange(-1.0*dt,25.0*dt,dt)
+y = []
+u = []
+e = []
+
+y0 = 0
 
 
+# Kp = int(input("Kp "))
+# Ki = int(input("Ki "))
+# Kd = int(input("Kd "))
 
-Kp = int(input("Kp "))
-Ki = int(input("Ki "))
-Kd = int(input("Kd "))
-
+print(t)
 
 
 
